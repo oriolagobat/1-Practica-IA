a74affa3551e5af05ddd8d23861479c18f86ac37
Ferran Aran Domingo <farandomingo@gmail.com>
2021-10-18 17:31:50 +0200

    Initial commit

---
+++ search/node.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, _state, _parent=None, _action=None, _cost=0):
+        self.state = _state
+        self.parent = _parent
+        self.action = _action
+        self.cost = _cost
+
+    def total_path_rec(self):
+        if not self.parent:
+            return []
+        return self.parent.total_path_rec() + [self.action]
+
+    def total_path(self):
+        current_state = self
+        actions = []
+        while current_state.parent:
+            actions += [current_state.action]
+            current_state = current_state.parent
+        return actions[::-1]
+
+    def __str__(self):
+        return "Node[%s,%s,%d]" % (self.state, self.action, self.cost)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def test_robot():
+    # For the problem of the robot cleaning two cells
+    from collections import namedtuple
+    State = namedtuple("State", "cell clean0 clean1")  # Tenemos el estado aqui, 1: Dnde esta el robot 2 y 3: estado
+    # de las dos celdas.
+    root = Node(State(0, False, False))
+    step1 = Node(
+        State(0, True, False),
+        root, "SWEEP"
+    )
+    step2 = Node(
+        State(1, True, False),
+        step1, "MOVE"
+    )
+    step3_1 = Node(
+        State(0, True, False),
+        step2, "MOVE"
+    )
+    step3_2 = Node(
+        State(1, True, True),
+        step2, "SWEEP"
+    )
+
+    print(root)
+    print([root, step1])
+    print(step3_1.total_path())
+    # print(step3_2.total_path())
+
+
+if __name__ == "__main__":
+    test_robot()
